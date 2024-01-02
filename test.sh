d0677b6b532c16963ca0055e79bbd1ac6881011d
mich101mich <mich101mich@gmail.com>
2024-01-02 20:41:40 +0100

    Fixed tests

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+OVERWRITE=0
+if [[ "$1" == "overwrite" ]]; then
+    OVERWRITE=1
+elif [[ -n "$1" ]]; then
+    echo "Usage: $0 [overwrite]"
+    exit 1
+fi
+
 TMP_FILE="/tmp/sscanf_test_out.txt"
 
 function handle_output {
@@ -9,9 +17,9 @@ function handle_output {
         echo "${line}" >> "${TMP_FILE}"
 
         # make sure line is not longer than the terminal width
-        width=$(tput cols) # read this again in case the terminal was resized
-        width=$((width - 3)) # leave space for the "..."
-        TRIMMED_LINE=$(echo "> ${line}" | sed "s/\(.\{${width}\}\).*/\1.../")
+        WIDTH=$(tput cols) # read this again in case the terminal was resized
+        WIDTH=$((WIDTH - 3)) # leave space for the "..."
+        TRIMMED_LINE=$(echo "> ${line}" | sed "s/\(.\{${WIDTH}\}\).*/\1.../")
         echo -en "\033[2K\r${TRIMMED_LINE}"
         tput init # trimmed line may have messed up coloring
     done
@@ -47,8 +55,17 @@ done
     try_silent cargo update || exit 1
     try_silent cargo +stable test || exit 1
     try_silent cargo +nightly test || exit 1
-    try_silent cargo +stable test error_message_tests -- --ignored || exit 1
-    try_silent cargo +nightly test error_message_tests -- --ignored || exit 1
+
+    if [[ OVERWRITE -eq 1 ]]; then
+        echo "Trybuild overwrite mode enabled"
+        export TRYBUILD=overwrite
+        try_silent cargo +stable test error_message_tests -- --ignored || exit 1
+        git add tests/fail # stage overwrite changes first, in case `nightly` would undo them
+        try_silent cargo +nightly test error_message_tests -- --ignored || exit 1
+    else
+        try_silent cargo +stable test error_message_tests -- --ignored || exit 1
+        try_silent cargo +nightly test error_message_tests -- --ignored || exit 1
+    fi
     export RUSTDOCFLAGS="-D warnings"
     try_silent cargo +nightly doc --no-deps || exit 1
     try_silent cargo +nightly clippy -- -D warnings || exit 1
@@ -75,3 +92,5 @@ done
     try_silent cargo +stable test || exit 1
     try_silent cargo +nightly test || exit 1
 ) || exit 1
+
+echo "All tests passed!"
