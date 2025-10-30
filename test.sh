ed87fc0cc79e0fbb0ea11d5a700bc6b86a9728ff
mich101mich <mich101mich@gmail.com>
2025-10-30 18:44:40 +0100

    Removed upper bounds for dependencies

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 base_dir="$(realpath "$(dirname "$0")")"
 sub_directories="sscanf_macro"
 is_proc_macro=1
+msrv_overrides="quote@1.0.40 glob@0.3.2 proc-macro2@1.0.101 const_format@0.2.31 unicode-width@0.1.12"
 
-export base_dir sub_directories is_proc_macro
+export base_dir sub_directories is_proc_macro msrv_overrides
 
 "${base_dir}/submodules/test_script/test.sh" "$@"
